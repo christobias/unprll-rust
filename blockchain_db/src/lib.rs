307a4676cae33d971e03080cadc04789919398c6
Christopher Tobias <ctobias@gmx.com>
2019-08-25 07:21:08 +0530

    Update Blockchain DB interface
    Certain operations are common across all DB types so it makes more sense 
    to abstract just the DB interfacing to a driver struct and move the 
    blockchain logic to this common behavior struct

--- blockchain_db/src/lib.rs
+++ blockchain_db/src/lib.rs
@@ -1,48 +1,160 @@
 #[macro_use] extern crate failure;
 
-use crypto::Hash256;
-use common::{Block, Transaction};
+use crypto::{
+    Hash256,
+    KeyImage
+};
+use common::{
+    Block,
+    Config,
+    GetHash,
+    PreliminaryChecks,
+    Transaction,
+    TXIn
+};
 
 mod error;
-mod lmdb;
+// mod lmdb;
 mod mem;
 
 pub use error::Result;
-pub use lmdb::BlockchainLMDB;
-pub use mem::BlockchainMemDB;
 
-pub trait BlockchainDB {
+/// Manages communication between the database and the rest of the application.
+trait BlockchainDBDriver {
     // DB Operations
     fn is_read_only(&self) -> bool;
-    fn sync(&self);
+    fn sync(&self) -> Result<()>;
     fn set_safe_sync_mode(&self, state: bool);
     fn reset(&mut self);
     fn size(&self) -> u64;
     fn fixup(&self);
 
-    // Batch Operations
-    // fn start_batch() -> Result<()>;
-    // fn stop_batch() -> Result<()>;
-
     // Block
-    fn add_block(&mut self, block: Block, block_weight: usize, cumulative_difficulty: usize, coins_generated: u64, transactions: Vec<Transaction>) -> Result<()>;
-    fn get_block_by_height(&self, height: u64) -> Result<Block>;
-    fn get_block_by_hash(&self, block_id: &Hash256) -> Result<Block>;
-    fn get_cumulative_difficulty(&self) -> u64;
+    fn add_block(&mut self, block: Block) -> Result<()>;
+    fn get_block_by_height(&self, height: u64) -> Option<Block>;
+    fn get_block_by_hash(&self, block_id: &Hash256) -> Option<Block>;
+
     // Zero index height, for consistency
-    fn get_height(&self) -> u64;
+    fn get_tail(&self) -> Result<(u64, Block)>;
     fn pop_block(&mut self) -> Result<Block>;
 
-    // Confirmed Transactions
-    fn add_transaction(&mut self);
-    fn get_transaction(&self, id: &Hash256) -> Result<Transaction>;
+    fn get_cumulative_difficulty(&self) -> u64;
 
-    // Unconfirmed Transactions
-    fn add_txpool_transaction(&mut self);
-    fn get_txpool_transaction(&self);
-    fn get_txpool_transaction_count(&self);
-    fn remove_txpool_transaction(&mut self);
+    // Confirmed Transactions
+    fn add_transaction(&mut self, transaction: Transaction) -> Result<()>;
+    fn get_transaction(&self, id: &Hash256) -> Option<Transaction>;
 
     // Key Image
-    fn has_key_image(&self);
+    fn add_key_image(&mut self, key_image: KeyImage) -> Result<()>;
+    fn has_key_image(&self, key_image: &KeyImage) -> bool;
+}
+
+/// # Blockchain database
+///
+/// This struct interfaces between users and db "drivers" and maintains the following policy:
+///
+/// 1. Adding a new block requires that it, it's transactions, and all key images in each
+///    transaction don't exist already
+/// 2. Each block connects to its parent
+/// 3. Confirmed transactions can only be added via blocks
+pub struct BlockchainDB {
+    db: Box<dyn BlockchainDBDriver + Sync + Send>
+}
+
+impl BlockchainDB {
+    pub fn new(config: &Config) -> Self {
+        BlockchainDB {
+            db: match config.db_type.as_ref() {
+                "memory" => Box::new(mem::BlockchainMemDB::new()),
+                _ => panic!()
+            }
+        }
+    }
+
+    pub fn add_block(&mut self, block: Block, transactions: Vec<Transaction>) -> Result<()> {
+        // Do preliminary checks
+        self.check(&block)?;
+        for tx in transactions.iter() {
+            self.check(tx)?;
+        }
+        // Then insert everything
+        self.db.add_transaction(block.miner_tx.clone())?;
+        for tx in transactions.into_iter() {
+            for input in tx.prefix.inputs.iter() {
+                if let TXIn::FromKey{key_image, ..} = input {
+                    self.db.add_key_image(*key_image)?;
+                }
+            }
+            self.db.add_transaction(tx)?;
+        }
+
+        self.db.add_block(block)
+    }
+
+    // Passthrough
+    pub fn get_block_by_height(&self, height: u64) -> Option<Block> { self.db.get_block_by_height(height) }
+    pub fn get_block_by_hash(&self, hash: &Hash256) -> Option<Block> { self.db.get_block_by_hash(hash) }
+    pub fn get_tail(&self) -> Result<(u64, Block)> { self.db.get_tail() }
+}
+
+impl PreliminaryChecks<Block> for BlockchainDB {
+    fn check(&self, block: &Block) -> Result<()> {
+        let block_id = block.get_hash();
+        // Verify that:
+
+        let height;
+
+        if let TXIn::Gen{height: h} = block.miner_tx.prefix.inputs[0] {
+            height = h;
+        } else {
+            return Err(format_err!("Block with ID {} does not have a genesis tx input", block_id));
+        }
+
+        // 1. This new block connects to our existing chain
+        //                    - or -
+        //    This block's height is 0 and connects to null
+        let tail = self.db.get_tail();
+        if tail.is_ok() {
+            let (_, tail_block) = tail.unwrap();
+            if tail_block.get_hash() != block.header.prev_id {
+                return Err(format_err!("Block with ID {} does not connect to our chain (Our chain tail: {}, block's previous ID: {})", block_id, tail_block.get_hash(), block.header.prev_id));
+            }
+        } else if block.header.prev_id != Hash256::null_hash() {
+            return Err(format_err!("Block with ID {} (supposed to be our genesis block) does not have a previous ID of {}", block_id, Hash256::null_hash()));
+        } else if height != 0 {
+            return Err(format_err!("Block with ID {} (supposed to be our genesis block) does not have a height of 0", block_id));
+        }
+
+        // 2. We don't have that block already
+        if self.db.get_block_by_hash(&block_id).is_some() {
+            return Err(format_err!("Block with ID {} exists", block_id));
+        }
+
+        // 3. It doesn't overwrite a block on an existing height
+        if self.db.get_block_by_height(height).is_some() {
+            return Err(format_err!("Block at height {} exists", height));
+        }
+
+        Ok(())
+    }
+}
+
+impl PreliminaryChecks<Transaction> for BlockchainDB {
+    fn check(&self, transaction: &Transaction) -> Result<()> {
+        let txid = transaction.get_hash();
+        // 4. We don't have any of the transactions already
+        if self.db.get_transaction(&txid).is_some() {
+            return Err(format_err!("Transaction with ID {} exists", txid));
+        }
+
+        for input in transaction.prefix.inputs.iter() {
+            if let TXIn::FromKey{key_image, ..} = input {
+                // 5. We don't have any of the key images already
+                if self.db.has_key_image(key_image) {
+                    return Err(format_err!("Key image {:?} exists", *key_image));
+                }
+            }
+        }
+        Ok(())
+    }
 }
--- blockchain_db/src/mem.rs
+++ blockchain_db/src/mem.rs
@@ -1,16 +1,25 @@
 use std::collections::HashMap;
 use std::convert::TryInto;
 
-use common::{Block, GetHash, Transaction};
-use crypto::Hash256;
+use common::{
+    Block,
+    GetHash,
+    Transaction
+};
+use crypto::{
+    Hash256,
+    KeyImage
+};
 
 use crate::error::Result;
-use crate::BlockchainDB;
+use crate::BlockchainDBDriver;
 
 pub struct BlockchainMemDB {
     blocks: HashMap<Hash256, Block>,
     block_heights: HashMap<u64, Hash256>,
-    transactions: HashMap<Hash256, Transaction>
+    key_images: Vec<KeyImage>,
+    transactions: HashMap<Hash256, Transaction>,
+    unconfirmed_transactions: HashMap<Hash256, Transaction>
 }
 
 impl BlockchainMemDB {
@@ -18,17 +27,19 @@ impl BlockchainMemDB {
         BlockchainMemDB {
             blocks: HashMap::new(),
             block_heights: HashMap::new(),
-            transactions: HashMap::new()
+            key_images: Vec::new(),
+            transactions: HashMap::new(),
+            unconfirmed_transactions: HashMap::new()
         }
     }
 }
 
-impl BlockchainDB for BlockchainMemDB {
+impl BlockchainDBDriver for BlockchainMemDB {
     fn is_read_only(&self) -> bool {
         false
     }
-    fn sync(&self) {
-
+    fn sync(&self) -> Result<()> {
+        Ok(())
     }
     fn set_safe_sync_mode(&self, _state: bool) {
 
@@ -36,7 +47,9 @@ impl BlockchainDB for BlockchainMemDB {
     fn reset(&mut self) {
         self.blocks.clear();
         self.block_heights.clear();
+        self.key_images.clear();
         self.transactions.clear();
+        self.unconfirmed_transactions.clear();
     }
     fn size(&self) -> u64 {
         std::mem::size_of_val(&self).try_into().unwrap_or(u64::max_value())
@@ -46,71 +59,51 @@ impl BlockchainDB for BlockchainMemDB {
     }
 
     // Block
-    fn add_block(&mut self, block: Block, _block_weight: usize, _cumulative_difficulty: usize, _coins_generated: u64, transactions: Vec<Transaction>) -> Result<()> {
-        let block_id = &block.get_hash();
-        if self.blocks.contains_key(block_id) {
-            return Err(format_err!("Block with ID {} exists", block_id));
-        }
-
-        for tx in transactions.iter() {
-            let txid = &tx.get_hash();
-            if self.transactions.contains_key(txid) {
-                return Err(format_err!("Transaction with ID {} exists", txid));
-            }
-        }
-
-        self.blocks.insert(block_id.to_owned(), block);
-        for tx in transactions {
-            let txid = &tx.get_hash();
-            self.transactions.insert(txid.to_owned(), tx);
-        }
+    fn add_block(&mut self, block: Block) -> Result<()> {
+        let block_id = block.get_hash();
+        self.blocks.insert(block_id.clone(), block);
+        self.block_heights.insert(self.get_tail().map(|(current_height, _)| current_height + 1).unwrap_or(0), block_id);
         Ok(())
     }
-    fn get_block_by_height(&self, height: u64) -> Result<Block> {
-        let block_id = self.block_heights.get(&height).ok_or(format_err!("Block at height {} does not exist", height))?;
+    fn get_block_by_height(&self, height: u64) -> Option<Block> {
+        let block_id = self.block_heights.get(&height)?;
         self.get_block_by_hash(block_id)
     }
-    fn get_block_by_hash(&self, block_id: &Hash256) -> Result<Block> {
-        self.blocks.get(&block_id).cloned().ok_or(format_err!("Block with ID {:?} does not exist", block_id))
+    fn get_block_by_hash(&self, block_id: &Hash256) -> Option<Block> {
+        self.blocks.get(&block_id).cloned()
     }
     fn get_cumulative_difficulty(&self) -> u64 {
+        // TODO:
         100
     }
-    fn get_height(&self) -> u64 {
-        (self.block_heights.len() - 1) as u64
+    fn get_tail(&self) -> Result<(u64, Block)> {
+        let mut height: u64 = self.block_heights.iter().count().try_into().unwrap();
+        if height != 0 { height -= 1 }
+
+        Ok((height, self.get_block_by_height(height).ok_or_else(|| format_err!("Chain does not have any blocks"))?))
     }
     fn pop_block(&mut self) -> Result<Block> {
-        let height = self.get_height();
-        let block_id = self.block_heights.get(&height).ok_or(format_err!("Block at height {} does not exist", height))?;
+        let (height, _) = self.get_tail()?;
+        let block_id = self.block_heights.get(&height).ok_or_else(|| format_err!("Block at height {} does not exist", height))?;
 
         // At this point, it can be assumed the block exists on both tables
-        let block = self.blocks.remove_entry(block_id).expect("Inconsistent state").1;
+        let (_, block) = self.blocks.remove_entry(block_id).expect("Inconsistent state");
         Ok(block)
     }
 
-    // Confirmed Transactions
-    fn add_transaction(&mut self) {
-
+    fn add_transaction(&mut self, transaction: Transaction) -> Result<()> {
+        self.transactions.insert(transaction.get_hash(), transaction);
+        Ok(())
     }
-    fn get_transaction(&self, id: &Hash256) -> Result<Transaction> {
-        self.transactions.get(id).cloned().ok_or(format_err!("Transaction with ID {:?} does not exist", id))
-    }
-
-    // Unconfirmed Transactions
-    fn add_txpool_transaction(&mut self) {
-
-    }
-    fn get_txpool_transaction(&self) {
-
-    }
-    fn get_txpool_transaction_count(&self) {
-
-    }
-    fn remove_txpool_transaction(&mut self) {
-
+    fn get_transaction(&self, id: &Hash256) -> Option<Transaction> {
+        self.transactions.get(id).cloned()
     }
 
-    fn has_key_image(&self) {
-
+    fn add_key_image(&mut self, key_image: KeyImage) -> Result<()> {
+        self.key_images.push(key_image);
+        Ok(())
+    }
+    fn has_key_image(&self, key_image: &KeyImage) -> bool {
+        self.key_images.contains(key_image)
     }
 }
